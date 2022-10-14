df49908962050663d063ae57b41c74083c8ef115
David <david.santos54@fatec.sp.gov.br>
2022-10-14 15:38:45 -0300

    Exercício 04 - Sistema de Estoque

---
+++ Exercicio04/Exercicio04/Entidades/Produto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercicio04.Entidades
+{
+    public class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value;
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set => _preco = value;
+        }
+
+        public int Quantidade => _quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            if (_quantidade - quantidade >= 0)
+            {
+                _quantidade -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine("ERRO! Não é possível ter uma quantidade negativa em estoque!");
+            }
+        }
+    }
+}
---
+++ Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Exercicio04.Entidades;
+
+namespace Exercicio04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+            
+            Console.WriteLine("Entre com os dados dos produtos:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = Double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            produto.AdicionarProdutos(Int32.Parse(Console.ReadLine()));
+            
+            Console.WriteLine($"Dados do Produto: {produto.Nome}, $ {produto.Preco}, {produto.Quantidade} unidades, Total: $ {produto.ValorTotalEmEstoque()}");
+            
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(Int32.Parse(Console.ReadLine()));
+            
+            Console.WriteLine($"Dados do Atualizados: {produto.Nome}, $ {produto.Preco}, {produto.Quantidade} unidades, Total: $ {produto.ValorTotalEmEstoque()}");
+            
+            Console.Write("Digite o número de produtos a ser removidos do estoque: ");
+            produto.RemoverProdutos(Int32.Parse(Console.ReadLine()));
+            
+            Console.WriteLine($"Dados do Atualizados: {produto.Nome}, $ {produto.Preco}, {produto.Quantidade} unidades, Total: $ {produto.ValorTotalEmEstoque()}");
+        }
+    }
+}
