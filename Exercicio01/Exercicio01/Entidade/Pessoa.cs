5c9fffee3f983aebb57d3c08b5b9315f09a7aa86
David <david.santos54@fatec.sp.gov.br>
2022-10-14 12:19:08 -0300

    Exercício 01 - Quem é mais velho?

---
+++ Exercicio01/Exercicio01/Entidade/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio01.Entidade
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        
+        public int Idade
+        {
+            get { return _idade; }
+            set { _idade = value; }
+        }
+    }
+}
---
+++ Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Exercicio01.Entidade;
+
+namespace Exercicio01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+            
+            Console.Write("Informe o nome da primeira pessoa: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.Write("Informe a idade da primeira pessoa: ");
+            pessoa1.Idade = Int32.Parse(Console.ReadLine());
+            
+            Console.Write("Informe o nome da segunda pessoa: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.Write("Informe a idade da segunda pessoa: ");
+            pessoa2.Idade = Int32.Parse(Console.ReadLine());
+
+            if (pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine($"A pessoa mais velha é: {pessoa1.Nome}");
+            }
+            else if(pessoa1.Idade < pessoa2.Idade)
+            {
+                Console.WriteLine($"A pessoa mais velha é: {pessoa2.Nome}");
+            }
+            else
+            {
+                Console.WriteLine("As duas pessoas tem a mesma idade!");
+            }
+        }
+    }
+}
