2e3535b61bae15162efb12fc7ab74a2b98b6f742
David <david.santos54@fatec.sp.gov.br>
2022-10-14 14:16:21 -0300

    Exercício 03 - Área do Triângulo

---
+++ Exercicio03/Exercicio03/Entidades/Triangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio03.Entidades
+{
+    public class Triangulo
+    {
+        private double _ladoA;
+        private double _ladoB;
+        private double _ladoC;
+
+        public double LadoA
+        {
+            get => _ladoA;
+            set => _ladoA = value;
+        }
+
+        public double LadoB
+        {
+            get => _ladoB;
+            set => _ladoB = value;
+        }
+
+        public double LadoC
+        {
+            get => _ladoC;
+            set => _ladoC = value;
+        }
+
+        public double Area()
+        {
+            double p = (_ladoA + _ladoB + _ladoC) / 2.0;
+            return Math.Sqrt(p * (p - _ladoA) * (p - _ladoB) * (p - _ladoC));
+
+        }
+    }
+}
---
+++ Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Exercicio03.Entidades;
+
+namespace Exercicio03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Triangulo triangulo1 = new Triangulo();
+            Triangulo triangulo2 = new Triangulo();
+            
+            Console.Write("Entre com o valor do primeiro lado do primeiro triângulo");
+            triangulo1.LadoA = Double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor do segundo lado do primeiro triângulo");
+            triangulo1.LadoB = Double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor do terceiro lado do primeiro triângulo");
+            triangulo1.LadoC = Double.Parse(Console.ReadLine());
+            
+            Console.Write("Entre com o valor do primeiro lado do segundo triângulo");
+            triangulo2.LadoA = Double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor do segundo lado do segundo triângulo");
+            triangulo2.LadoB = Double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor do terceiro lado do segundo triângulo");
+            triangulo2.LadoC = Double.Parse(Console.ReadLine());
+
+            double areaPrimeiroTriangulo = triangulo1.Area();
+            double areaSegundoTriangulo = triangulo2.Area();
+            
+            Console.WriteLine($"Área Primeiro Triângulo: {areaPrimeiroTriangulo}");
+            Console.WriteLine($"Área Segundo Triângulo: {areaSegundoTriangulo}");
+
+            if (areaPrimeiroTriangulo > areaSegundoTriangulo)
+            {
+                Console.WriteLine("A área do primeiro triângulo é maior que a do segundo");
+            }
+            else if (areaPrimeiroTriangulo < areaSegundoTriangulo)
+            {
+                Console.WriteLine("A área do segundo triâgulo é maior que a do primeiro");
+            }
+            else
+            {
+                Console.WriteLine("Ambos triângulos possuem a mesma área!");
+            }
+        }
+    }
+}
