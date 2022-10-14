3abb03fa607e511e9e5cae54d36b0e4edf02780a
David <david.santos54@fatec.sp.gov.br>
2022-10-14 19:24:55 -0300

    Exercício 06 - Salário de Funcionários

---
+++ Exercicio06/Exercicio06/Entidades/Funcionario.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercicio06.Entidades
+{
+    public class Funcionario
+    {
+        private string _nome;
+        private double _salarioBruto;
+        private double _imposto;
+
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value;
+        }
+
+        public double SalarioBruto
+        {
+            get => _salarioBruto;
+            set => _salarioBruto = value;
+        }
+
+        public double Imposto
+        {
+            get => _imposto;
+            set => _imposto = value;
+        }
+
+        public double SalarioLiquido()
+        {
+            return _salarioBruto - _imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            _salarioBruto *= 1 + porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"Funcionário = {_nome}, $ {SalarioLiquido()}";
+        }
+    }
+}
---
+++ Exercicio06/Exercicio06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Exercicio06.Entidades;
+
+namespace Exercicio06
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = Double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine(funcionario);
+            
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            funcionario.AumentarSalario(Double.Parse(Console.ReadLine()));
+            
+            Console.WriteLine($"Dados Atualizados: {funcionario.Nome}, $ {funcionario.SalarioLiquido()}");
+        }
+    }
+}
