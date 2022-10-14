4f3e498a6fa4223b14aa0ebc5e3cfa77e16d3ed9
David <david.santos54@fatec.sp.gov.br>
2022-10-14 19:04:35 -0300

    Exercício 05 - Área, Perímetro e Diagonal de um retângulo

---
+++ Exercicio05/Exercicio05/Entidades/Retangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio05.Entidades
+{
+    public class Retangulo
+    {
+        private double _largura;
+        private double _altura;
+
+        public double Largura
+        {
+            get => _largura;
+            set => _largura = value;
+        }
+
+        public double Altura
+        {
+            get => _altura;
+            set => _altura = value;
+        }
+
+        public double Area()
+        {
+            return _largura * _altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (_altura + _largura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(_altura, 2) + Math.Pow(_largura, 2));
+        }
+        
+    }
+}
---
+++ Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Exercicio05.Entidades;
+
+namespace Exercicio05
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            
+            Console.Write("Informe a largura do retangulo: ");
+            retangulo.Largura = Double.Parse(Console.ReadLine());
+            
+            Console.Write("Informe a altura do retangulo: ");
+            retangulo.Altura = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"ÁREA = {retangulo.Area()}");
+            Console.WriteLine($"PERÍMETRO = {retangulo.Perimetro()}");
+            Console.WriteLine($"DIAGONAL = {retangulo.Diagonal()}");
+        }
+    }
+}
