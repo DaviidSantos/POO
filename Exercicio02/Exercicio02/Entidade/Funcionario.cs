3084a73311773157f0788d9e7d07e8b0d0b32a89
David <david.santos54@fatec.sp.gov.br>
2022-10-14 12:27:33 -0300

    Exercicio 02 - Média salarial

---
+++ Exercicio02/Exercicio02/Entidade/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio02.Entidade
+{
+    public class Funcionario
+    {
+        private string _nome;
+        private double _salario;
+
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value;
+        }
+
+        public double Salario
+        {
+            get => _salario;
+            set => _salario = value;
+        }
+    }
+}
---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Exercicio02.Entidade;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        public const int NUMERO_DE_FUNCIONARIOS = 2;
+        public static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+            
+            Console.Write("Informe o nome do primeiro funcionário: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Informe o salario do primeiro funcionario: ");
+            funcionario1.Salario = double.Parse(Console.ReadLine());
+            
+            Console.Write("Informe o nome do segundo funcionário: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Informe o salario do segundo funcionario: ");
+            funcionario2.Salario = double.Parse(Console.ReadLine());
+
+            double media = (funcionario1.Salario + funcionario2.Salario) / NUMERO_DE_FUNCIONARIOS;
+            
+            Console.WriteLine($"A média salarial dos funcionarios é de R$ {media}");
+        }
+    }
+}
