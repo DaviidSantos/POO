a6adb16395f9374d95c4540eaeeaab1fec3d04e6
David <david.santos54@fatec.sp.gov.br>
2022-10-15 18:34:11 -0300

    Exercício 07 - Nota Final Aluno

---
+++ Exercicio07/Exercicio07/Entidades/Aluno.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Exercicio07.Entidades
+{
+    public class Aluno
+    {
+        private const int NOTA_MINIMA = 60;
+        private string _nome;
+        private double _primeiroTrimestre;
+        private double _segundoTrimestre;
+        private double _terceiroTrimestre;
+        private double _notaFinal;
+        private string _conceito;
+
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value;
+        }
+
+        public double PrimeiroTrimestre
+        {
+            get => _primeiroTrimestre;
+            set
+            {
+                if (value >= 0 && value <= 30)
+                {
+                    _primeiroTrimestre = value;
+                }
+                else
+                {
+                    Console.WriteLine("A nota do primeiro trimestre tem que estar entre 0 e 30");
+                }
+            }
+        }
+
+        public double SegundoTrimestre
+        {
+            get => _segundoTrimestre;
+            set
+            {
+                if (value >= 0 && value <= 35)
+                {
+                    _segundoTrimestre = value;
+                }
+                else
+                {
+                    Console.WriteLine("A nota do segundo trimestre tem que estar entre 0 e 35");
+                }
+            } 
+        }
+        
+        public double TerceiroTrimestre
+        {
+            get => _terceiroTrimestre;
+            set
+            {
+                if (value >= 0 && value <= 35)
+                {
+                    _terceiroTrimestre = value;
+                }
+                else
+                {
+                    Console.WriteLine("A nota do terceiro trimestre tem que estar entre 0 e 35");
+                }
+            } 
+        }
+        
+
+        public double NotaFinal()
+        {
+            _notaFinal = _primeiroTrimestre + _segundoTrimestre + _terceiroTrimestre;
+            return _notaFinal;
+        }
+
+        public void ConceitoFinal()
+        {
+            if (_notaFinal >= 60)
+            {
+                _conceito = "Aprovado";
+                Console.WriteLine(_conceito);
+            }
+            else
+            {
+                double pontosFaltantes;
+                
+                _conceito = "Reprovado";
+                Console.WriteLine(_conceito);
+
+                pontosFaltantes = NOTA_MINIMA - _notaFinal;
+                Console.WriteLine($"Faltaram {pontosFaltantes} pontos!");
+            }
+        }
+    }
+}
---
+++ Exercicio07/Exercicio07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Exercicio07.Entidades;
+
+namespace Exercicio07
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            
+            Console.Write("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.Write("Digite a nota do primeiro trimestre: ");
+            aluno.PrimeiroTrimestre = Double.Parse(Console.ReadLine());
+            Console.Write("Digite a nota do segundo trimestre: ");
+            aluno.SegundoTrimestre = Double.Parse(Console.ReadLine());
+            Console.Write("Digite a nota do terceiro trimestre: ");
+            aluno.TerceiroTrimestre = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"NOTA FINAL = {aluno.NotaFinal()}");
+            aluno.ConceitoFinal();
+            
+        }
+    }
+}
