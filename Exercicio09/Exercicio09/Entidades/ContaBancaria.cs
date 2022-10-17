e8c66e7fb6abbffba4681595e81eafe66fdbf518
David <david.santos54@fatec.sp.gov.br>
2022-10-17 13:48:30 -0300

    Exercicio 09 - Conta Bancária

---
+++ Exercicio09/Exercicio09/Entidades/ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercicio09.Entidades
+{
+    public class ContaBancaria
+    {
+        private const double TAXA_SAQUE = 5.00;
+        private int _numeroDaConta;
+        private string _titularDaConta;
+        private double _saldo;
+
+        public ContaBancaria(int numeroDaConta, string titularDaConta)
+        {
+            _numeroDaConta = numeroDaConta;
+            _titularDaConta = titularDaConta;
+        }
+
+        public ContaBancaria(int numeroDaConta, string titularDaConta, double saldo) : this(numeroDaConta, titularDaConta)
+        {
+            _saldo = saldo;
+        }
+
+        public int NumeroDaConta => _numeroDaConta;
+
+        public string TitularDaConta
+        {
+            get => _titularDaConta;
+            set => _titularDaConta = value;
+        }
+
+        public double Saldo => _saldo;
+
+        public void Deposito(double valorDoDeposito)
+        {
+            _saldo += valorDoDeposito;
+        }
+
+        public void Saque(double valorDoSaque)
+        {
+            _saldo -= valorDoSaque + TAXA_SAQUE;
+        }
+
+        public override string ToString()
+        {
+            return $"\nDados da Conta:\nConta {_numeroDaConta}, Titular: {_titularDaConta}, Saldo: $ {_saldo.ToString("N2")}\n";
+        }
+    }
+}
---
+++ Exercicio09/Exercicio09/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Exercicio09.Entidades;
+
+namespace Exercicio09
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numeroDaConta;
+            string titularDaConta;
+            double depositoInicial;
+            char haveraDepositoInicial;
+            
+            Console.Write("Entre o número da conta: ");
+            numeroDaConta = Int32.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            titularDaConta = Console.ReadLine();
+            Console.Write("Haverá depósito inicial?(s/n): ");
+            haveraDepositoInicial = Console.ReadKey().KeyChar;
+
+            if (haveraDepositoInicial == 's')
+            {
+                Console.Write("\nEntre o valor de depósito inicial: ");
+                depositoInicial = Double.Parse(Console.ReadLine());
+
+                ContaBancaria conta = new ContaBancaria(numeroDaConta, titularDaConta, depositoInicial);
+                Console.WriteLine(conta);
+                
+                Console.Write("Entre um valor para depósito: ");
+                conta.Deposito(Double.Parse(Console.ReadLine()));
+                Console.WriteLine(conta);
+                
+                Console.Write("Entre um valor para saque: ");
+                conta.Saque(Double.Parse(Console.ReadLine()));
+                Console.WriteLine(conta);
+            } 
+            else if (haveraDepositoInicial == 'n')
+            {
+                ContaBancaria conta = new ContaBancaria(numeroDaConta, titularDaConta);
+                Console.WriteLine(conta);
+                
+                Console.Write("Entre um valor para depósito: ");
+                conta.Deposito(Double.Parse(Console.ReadLine()));
+                Console.WriteLine(conta);
+                
+                Console.Write("Entre um valor para saque: ");
+                conta.Saque(Double.Parse(Console.ReadLine()));
+                Console.WriteLine(conta);
+            }
+            else
+            {
+                Console.WriteLine("Informe 's' ou 'n' para depósito inicial!");
+            }
+        }
+    }
+}
