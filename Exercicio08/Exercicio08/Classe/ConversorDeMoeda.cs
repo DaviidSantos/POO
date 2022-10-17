0dfd4708888ebff725474cfffeb577e7fe68c179
David <david.santos54@fatec.sp.gov.br>
2022-10-16 22:23:49 -0300

    Exercício 08 - Conversor de Moeda

---
+++ Exercicio08/Exercicio08/Classe/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio08.Classe
+{
+    public class ConversorDeMoeda
+    {
+        private const double IOF = 6.0;
+
+        public static double converterParaReais(double cotacao, double dolares)
+        {
+            return (dolares * (1 + IOF / 100)) * cotacao;
+        }
+    }
+}
---
+++ Exercicio08/Exercicio08/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Exercicio08.Classe;
+
+namespace Exercicio08
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar?: ");
+            double cotacao = Double.Parse(Console.ReadLine());
+            Console.Write("Quantos dolares você vai comprar?: ");
+            double dolares= Double.Parse(Console.ReadLine());
+            Console.WriteLine($"Valor a ser pago em reais: R$ {ConversorDeMoeda.converterParaReais(cotacao, dolares)}");
+        }
+    }
+}
