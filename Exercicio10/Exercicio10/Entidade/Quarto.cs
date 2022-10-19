2b7c4389e8d71c704e23697c1b540c39cb8d5e57
David <david.santos54@fatec.sp.gov.br>
2022-10-19 12:29:37 -0300

    Exercicio 10 - Aluguel de Quartos

---
+++ Exercicio10/Exercicio10/Entidade/Quarto.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio10.Entidade
+{
+    public class Quarto
+    {
+        private string _rentersName;
+        private string _email;
+        private int _roomNumber;
+
+        public string RentersName
+        {
+            get => _rentersName;
+            set => _rentersName = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public int RoomNumber
+        {
+            get => _roomNumber;
+            set => _roomNumber = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{_roomNumber}: {_rentersName}, {_email}";
+        }
+    }
+}
---
+++ Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Exercicio10.Entidade;
+
+namespace Exercicio10
+{
+    internal class Program
+    {
+        public static int NUMERO_DE_QUARTOS = 10;
+        
+        public static void Main(string[] args)
+        {
+            int numberOfRooms, roomNumber;
+            string name, email;
+
+            Console.Write("How many rooms will be rented: ");
+            numberOfRooms = Int32.Parse(Console.ReadLine());
+
+            Quarto[] quarto = new Quarto[NUMERO_DE_QUARTOS];
+
+            for (int i = 0; i < numberOfRooms; i++)
+            {
+                Console.WriteLine($"Rent #{i+1}");
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Room: ");
+                roomNumber = Int32.Parse(Console.ReadLine());
+
+                quarto[roomNumber] = new Quarto { RentersName = name, Email = email, RoomNumber = roomNumber };
+            }
+            
+            Console.WriteLine("Busy rooms:");
+            foreach (Quarto quartoOcupado in quarto)
+            {
+                Console.WriteLine(quartoOcupado);
+            }
+        }
+    }
+}
