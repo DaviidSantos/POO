c9978a21dbc43dec01285dfadc5c2f073d356441
David <david.santos54@fatec.sp.gov.br>
2022-10-19 13:29:24 -0300

    Exercício 11 - Salário de Funcionários (listas)

---
+++ Exercicio11/Exercicio11/Entidade/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercicio11.Entidade
+{
+    public class Funcionario
+    {
+        private int _id;
+        private string _name;
+        private double _salary;
+
+        public Funcionario(int id, string name, double salary)
+        {
+            _id = id;
+            _name = name;
+            _salary = salary;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _salary = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public double Salary
+        {
+            get => _salary;
+            set => _salary = value;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            _salary = _salary * (1 + percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{_id}, {_name}, {_salary}";
+        }
+    }
+}
---
+++ Exercicio11/Exercicio11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Principal;
+using Exercicio11.Entidade;
+
+namespace Exercicio11
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+            int quantidadeDeFuncionarios;
+            int id = 0;
+            double porcentagem;
+            
+            Console.Write("Quantos funcionários serão cadastrados?: ");
+            quantidadeDeFuncionarios = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidadeDeFuncionarios; i++)
+            {
+                string nome;
+                double salario;
+                
+                Console.WriteLine($"Funcionario #{i+1}:");
+                Console.Write("Id: ");
+                id = Int32.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                salario = Double.Parse(Console.ReadLine());
+                
+                funcionarios.Add(new Funcionario(id, nome, salario));
+            }
+            
+            Console.Write("Informe o id do funcionario que terá um aumento de salário: ");
+            id = Int32.Parse(Console.ReadLine());
+            Funcionario acharFuncionario = funcionarios.Find(x => x.Id == id);
+            if (acharFuncionario != null)
+            {
+                Console.Write("Informe a porcentagem: ");
+                porcentagem = Double.Parse(Console.ReadLine());
+                acharFuncionario.IncreaseSalary(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Funcionario não existe!");
+            }
+            
+            Console.WriteLine("Lista atualizada de funcionarios: ");
+            foreach (Funcionario funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario);
+            }
+        }
+    }
+}
