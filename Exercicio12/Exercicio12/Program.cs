9e23987176fa8088e66ede65ee656e7f56f23313
David <david.santos54@fatec.sp.gov.br>
2022-10-21 15:03:11 -0300

    Exercicio 12 - Matriz

---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Tamanho da matriz: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int negatives = 0;
+
+            int[,] mat = new int[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"mat[{i},{j}]");
+                    mat[i,j] = Int32.Parse(Console.ReadLine());
+                    if (mat[i,j] < 0)
+                    {
+                        negatives++;
+                    }
+                }
+            }
+
+            Console.Write("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{mat[i,i]} ");
+            }
+            
+            Console.Write($"\nNegative numbers: {negatives}");
+            
+        }
+    }
+}
